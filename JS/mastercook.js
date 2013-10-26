dfc8f0fe06a3c3b20b4918a29adf1d9abd2faddd
aaronpcz <aaronp261@gmail.com>
2013-10-26 21:52:41 +0100

    Added saving favourites.  With local increment, and a script to pull back a recipe given a certain recipe id.  This will be used in the diary to allow the most favourited recipe to be displayed.

--- JS/mastercook.js
+++ JS/mastercook.js
@@ -50,3 +50,53 @@ function rec2(data) {
 	})
 	}
 }
+
+function saveFavourite(recipeId)
+{
+	if(window.localStorage.getItem(recipeId) == null)
+	{
+		window.localStorage.setItem(recipeId, "1");
+	}
+	else
+	{
+		var count = window.localStorage.getItem(recipeId);
+		count = parseInt(count);
+		count += 1;
+		window.localStorage.setItem(recipeId, count.toString());
+	}
+}
+
+function retrieveFavourite(recipeId)
+{
+			var url = "http://api.yummly.com/v1/api/recipes?_app_id=7b053b82&_app_key=a2b4713bdb0b9b132a16a104823ed74c";
+
+			$.ajax({
+			type: 'GET',
+			url: url,
+			async: false,
+			crossDomain: true,
+			contentType: "application/json",
+			dataType: 'jsonp',
+			data:
+			{
+				q:recipeId,
+			},
+			success: function(response){ rec4(recipeId, response); },
+		});
+}
+
+function rec4(recipeId, data) {
+	if(data.matches.length == 0)
+	{
+		stuff.innerHTML = "No results...";
+	}
+	else
+	{
+		$.each( data.matches, function(key, value){
+			if(value.id == recipeId)
+			{
+				console.log(value);
+			}
+		})
+	}
+}
