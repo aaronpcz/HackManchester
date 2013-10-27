b38f2b565164b807893684b4e81eef3198c3ca1e
aaronpcz <aaronp261@gmail.com>
2013-10-27 01:24:48 +0000

    setting up local storage for flavours

--- JS/mastercook.js
+++ JS/mastercook.js
@@ -95,3 +95,105 @@ function rec4(recipeId, data) {
 		})
 	}
 }
+
+function retrieveFavouriteForCalculating(recipeId)
+{
+			var url = "http://api.yummly.com/v1/api/recipes?_app_id=7b053b82&_app_key=a2b4713bdb0b9b132a16a104823ed74c";
+
+			$.ajax({
+			type: 'GET',
+			url: url,
+			async: false,
+			crossDomain: true,
+			contentType: "application/json",
+			dataType: 'jsonp',
+			data:
+			{
+				q:recipeId,
+			},
+			success: function(response){ gettingRecipeObject(recipeId, response); },
+		});
+}
+
+function gettingRecipeObject(recipeId, data) {
+	if(data.matches.length == 0)
+	{
+		stuff.innerHTML = "No results...";
+	}
+	else
+	{
+		$.each( data.matches, function(key, value){
+			if(value.id == recipeId)
+			{
+				setupFlavour("bitter", value.flavors.bitter);
+				setupFlavour("meaty", value.flavors.meaty);
+				setupFlavour("salty", value.flavors.salty);
+				setupFlavour("sour", value.flavors.sour);
+				setupFlavour("sweet", value.flavors.sweet);
+			}
+		})
+	}
+}
+
+function setupFlavour(flavour, value)
+{
+	//
+	if(window.localStorage.getItem("bitter") == null)
+	{
+		window.localStorage.setItem("bitter", "0");
+	}
+	if(flavour == "bitter")
+	{
+		var calc = parseFloat(window.localStorage.getItem("bitter"));
+		calc = calc + value;
+		window.localStorage.setItem("bitter", calc);
+	}
+	
+	//
+	if(window.localStorage.getItem("meaty") == null)
+	{
+		window.localStorage.setItem("meaty", "0");
+	}
+	if(flavour == "meaty")
+	{
+		var calc = parseFloat(window.localStorage.getItem("meaty"));
+		calc = calc + value;
+		window.localStorage.setItem("meaty", calc);
+	}
+	
+	//
+	if(window.localStorage.getItem("salty") == null)
+	{
+		window.localStorage.setItem("salty", "0");
+	}
+	if(flavour == "salty")
+	{
+		var calc = parseFloat(window.localStorage.getItem("salty"));
+		calc = calc + value;
+		window.localStorage.setItem("salty", calc);
+	}
+	
+	//
+	if(window.localStorage.getItem("sour") == null)
+	{
+		window.localStorage.setItem("sour", "0");
+	}
+	if(flavour == "sour")
+	{
+		var calc = parseFloat(window.localStorage.getItem("sour"));
+		calc = calc + value;
+		window.localStorage.setItem("sour", calc);
+	}
+	
+	//
+	if(window.localStorage.getItem("sweet") == null)
+	{
+		window.localStorage.setItem("sweet", "0");
+	}
+	if(flavour == "sweet")
+	{
+		var calc = parseFloat(window.localStorage.getItem("sweet"));
+		calc = calc + value;
+		window.localStorage.setItem("sweet", calc);
+	}
+}
